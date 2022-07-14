ff02f248e459d2e6960c72638f1f2f1621d0af7c
fmeyer <fabian.meyer@inovex.de>
2022-07-14 17:28:27 +0200

    add custom caltech101

---
+++ src/ood_detection/datasets/caltech.py
@@ -0,0 +1,34 @@
+import os.path
+
+import torchvision.datasets
+from PIL import Image
+
+
+class StandardizedCaltech(torchvision.datasets.Caltech101):
+    def __init__(self, datapath, transform):
+        super().__init__(datapath,
+                         transform=transform,
+                         download=True)
+        self._labels = self.y
+        self._image_files = self.transform_to_image_list()
+
+    def __getitem__(self, idx):
+        img = Image.open(self._image_files[idx])
+        if self.transform is not None:
+            img = self.transform(img)
+        target = self._labels[idx]
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def transform_to_image_list(self):
+        # basically use the original __getitem__
+        file_list = []
+        for i in self.index:
+            path = os.path.join(self.root,
+                                "101_ObjectCategories",
+                                self.categories[self._labels[i]],
+                                f"image_{self.index[i]:04d}.jpg")
+            file_list.append(path)
+        return file_list
