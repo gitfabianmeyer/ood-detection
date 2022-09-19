bbdaf61044a47ecef48d62a8ac9686cc2e45cae1
fmeyer <fabian.meyer@inovex.de>
2022-09-19 17:58:23 +0200

    add store features script

---
+++ scripts/features/store_features.py
@@ -0,0 +1,66 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import clip
+import torch
+from ood_detection.config import Config
+from tqdm import tqdm
+from zoc.dataloaders.aircrat_loader import aircraft_single_isolated_class_loader
+from zoc.dataloaders.cars_loader import cars_single_isolated_class_loader
+from zoc.dataloaders.cifar10 import cifar10_single_isolated_class_loader
+from zoc.dataloaders.dtd_loader import dtd_single_isolated_class_loader
+from zoc.dataloaders.gtsrb_loader import gtrsb_single_isolated_class_loader
+from zoc.dataloaders.oxford3pets_loader import oxford3_single_isolated_class_loader
+
+device = Config.DEVICE
+batch_size = 128
+datasets = {'aircraft': aircraft_single_isolated_class_loader(batch_size),
+            'cars': cars_single_isolated_class_loader(batch_size),
+            'cifar10': cifar10_single_isolated_class_loader(batch_size),
+            'dtd': dtd_single_isolated_class_loader(batch_size),
+            'gtrsb': gtrsb_single_isolated_class_loader(batch_size),
+            'pets': oxford3_single_isolated_class_loader(batch_size)
+            }
+dataset_labels = []
+
+clip_model, preprocess = clip.load('Vit-B/32')
+for name, isolate_loader in datasets.items():
+
+    dataset_label = list(isolate_loader.keys())
+    base_path = os.path.join(Config.DATAPATH, 'features')
+    data_path = os.path.join(base_path, name)
+    for label in dataset_label:
+        loader = isolate_loader[loader]
+        label_path = os.path.join(data_path, label + '.pt')
+        os.makedirs(label_path, exist_ok=True)
+        with torch.no_grad():
+            features = []
+            for images, _ in tqdm(loader):
+                images = images.to(device)
+                batch_features = clip_model.encode_image(images)
+                batch_features /= batch_features.norm(dim=1, keepdim=True)
+                features.append(batch_features)
+
+            features = torch.cat(features)
+
+        torch.save(features, label_path)
+        print(f"Stored: {name} / {label} ({len(loader)} images) to {label_path}")
+
+    # encode and store labels
+
+    with torch.no_grad():
+        label_tokens = clip.tokenize(dataset_label).to(device)
+        label_features = clip_model.encode_text(label_tokens)
+        label_features /= label_features.norm(dim=1, keepdim=True)
+
+    labels_path = os.path.join(data_path, 'labels.pt')
+    torch.save(label_features, labels_path)
+    print(f"Stored label features for {name} ({len(dataset_label)} labels ) in {labels_path}")
+
+
+
+
+
+
