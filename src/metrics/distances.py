39668319c48dc2675654694384cffbe98208cb69
fmeyer <fabian.meyer@inovex.de>
2022-11-24 15:05:59 +0100

    add zeroshot accuracy, minor fixes

--- src/metrics/distances.py
+++ src/metrics/distances.py
@@ -120,7 +120,7 @@ class ZeroShotAccuracy(Distance):
 
     def get_distance(self):
         # do for the whole set
-        top1, top5 = classify(features=torch.stack(self.feature_dict.values),
+        top1, top5 = classify(features=torch.stack(self.feature_dict.values()),
                               zeroshot_weights=self.labels,
                               targets=self.dataset_targets)
         return top1
