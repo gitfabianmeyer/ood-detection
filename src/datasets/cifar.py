419260bcf7d88ef65d54212c4d4e333729c1f41f
fmeyer <fabian.meyer@inovex.de>
2022-11-28 15:16:22 +0100

    refactor

--- src/datasets/cifar.py
+++ src/datasets/cifar.py
@@ -10,21 +10,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 class OodCifar10(torchvision.datasets.CIFAR10):
-    def __init__(self, datapath, transform, train):
-        super(OodCifar10, self).__init__(root=datapath,
+    def __init__(self, data_path, transform, train):
+        super(OodCifar10, self).__init__(root=data_path,
                                          transform=transform,
                                          train=train,
                                          download=True
                                          )
-        self.targets = np.array(self.targets)
-
-
-class OodCifar100(torchvision.datasets.CIFAR100):
-    def __init__(self, datapath, transform, train):
-        super(OodCifar100, self).__init__(root=datapath,
-                                          transform=transform,
-                                          train=train,
-                                          download=True)
         self.targets = np.array(self.targets)
 
 
@@ -35,9 +26,6 @@ def main():
 
     cifar = OodCifar10(data_path, transform, train)
     get_distances_for_dataset(cifar, clip_model, "cifar10")
-
-    cifar = OodCifar100(data_path, transform, train)
-    get_distances_for_dataset(cifar, clip_model, "cifar100")
 
 
 if __name__ == '__main__':
---
+++ src/datasets/cifar100.py
@@ -0,0 +1,27 @@
+import clip
+import numpy as np
+import torchvision
+from metrics.distances import get_distances_for_dataset
+from ood_detection.config import Config
+
+
+class OodCifar100(torchvision.datasets.CIFAR100):
+    def __init__(self, data_path, transform, train):
+        super(OodCifar100, self).__init__(root=data_path,
+                                          transform=transform,
+                                          train=train,
+                                          download=True)
+        self.targets = np.array(self.targets)
+
+
+def main():
+    data_path = Config.DATAPATH
+    train = False
+    clip_model, transform = clip.load(Config.VISION_MODEL)
+
+    cifar = OodCifar100(data_path, transform, train)
+    get_distances_for_dataset(cifar, clip_model, "cifar100")
+
+
+if __name__ == '__main__':
+    main()
