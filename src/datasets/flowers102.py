2b9fdaa872dcae082cfa1f293b6fbd50d86cfda0
fmeyer <fabian.meyer@inovex.de>
2022-11-29 15:29:17 +0100

    debug full classification

--- src/datasets/flowers102.py
+++ src/datasets/flowers102.py
@@ -33,7 +33,6 @@ def main():
         print()
         prompter = pr
         dataset = OodFlowers102(data_path, transform, train, prompter)
-        dats
         print(dataset.classes[:2])
         # get_distances_for_dataset(dataset, clip_model, "caltech101")
         full_classification(dataset, clip_model, pr)
