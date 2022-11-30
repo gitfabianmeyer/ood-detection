59138bffca4332fc8c1bd42539dc0a77960646ec
fmeyer <fabian.meyer@inovex.de>
2022-11-30 17:21:08 +0100

    debug svhn

--- src/datasets/svhn.py
+++ src/datasets/svhn.py
@@ -33,8 +33,28 @@ class OodSVHN(torchvision.datasets.SVHN):
 def main():
     data_path = Config.DATAPATH
     train = False
-    clip_model, transform = clip.load(Config.VISION_MODEL)
+    # clip_model, transform = clip.load(Config.VISION_MODEL)
+    print('--------------------RN50x64-------------------------------')
 
+    clip_model, transform = clip.load('RN50x64')
+    dataset = OodSVHN(data_path, transform, train)
+    # get_distances_for_dataset(dataset, clip_model, "SVHN")
+    full_classification(dataset, clip_model, "SVHN BASE 0 - ZERO")
+
+    templates = ['a street sign of the number: "{}" ', ]
+    dataset = OodSVHN(data_path, transform, train, templates)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN TEMPLATE 0 - ZERO")
+
+    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    dataset = OodSVHN(data_path, transform, train, classes=classes)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN BASE 0")
+
+    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    dataset = OodSVHN(data_path, transform, train, templates=templates, classes=classes)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN TEMPLATE 0")
+
+    print('--------------------ViT-L/14-------------------------------')
+    clip_model, transform = clip.load('ViT-L/14')
     dataset = OodSVHN(data_path, transform, train)
     # get_distances_for_dataset(dataset, clip_model, "SVHN")
     full_classification(dataset, clip_model, "SVHN BASE 0 - ZERO")
