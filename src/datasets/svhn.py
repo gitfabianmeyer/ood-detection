4eadca1327d58ddf98764226f3ca4a2e98ffb682
fmeyer <fabian.meyer@inovex.de>
2022-11-30 17:09:54 +0100

    debug svhn

--- src/datasets/svhn.py
+++ src/datasets/svhn.py
@@ -5,6 +5,7 @@ import clip
 import torchvision.datasets
 
 from metrics.distances import get_distances_for_dataset
+from ood_detection.classification_utils import full_classification
 from ood_detection.config import Config
 from datasets.classnames import mnist_templates
 
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 class OodSVHN(torchvision.datasets.SVHN):
-    def __init__(self, data_path, transform, train, templates=None):
+    def __init__(self, data_path, transform, train, templates=None, classes=None):
         self.root = os.path.join(data_path, 'svhn')
         if not os.path.exists(self.root):
             os.mkdir(self.root)
@@ -21,9 +22,9 @@ class OodSVHN(torchvision.datasets.SVHN):
                                       download=True,
                                       split='train' if train else 'test')
         self.targets = self.labels
-        self.classes = ['0 - zero', '1 - one', '2 - two', '3 - three',
-                        '4 - four', '5 - five', '6 - six',
-                        '7 - seven', '8 - eight', '9 - nine']
+        self.classes = classes if classes else ['0 - zero', '1 - one', '2 - two', '3 - three',
+                                                '4 - four', '5 - five', '6 - six',
+                                                '7 - seven', '8 - eight', '9 - nine']
         self.class_to_idx = {self.classes[i]: i for i in range(10)}
         self.idx_to_class = {value: key for (key, value) in self.class_to_idx.items()}
         self.templates = templates if templates else mnist_templates
@@ -35,7 +36,20 @@ def main():
     clip_model, transform = clip.load(Config.VISION_MODEL)
 
     dataset = OodSVHN(data_path, transform, train)
-    get_distances_for_dataset(dataset, clip_model, "SVHN")
+    # get_distances_for_dataset(dataset, clip_model, "SVHN")
+    full_classification(dataset, clip_model, "SVHN BASE 0 - ZERO")
+
+    templates = ['a street sign of the number: "{}" ', ]
+    dataset = OodSVHN(data_path, transform, train, templates)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN TEMPLATE 0 - ZERO")
+
+    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    dataset = OodSVHN(data_path, transform, train, classes=classes)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN BASE 0")
+
+    classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    dataset = OodSVHN(data_path, transform, train, templates=templates, classes=classes)
+    full_classification(dataset, clip_model, "SVHN STREET SIGN TEMPLATE 0")
 
 
 if __name__ == '__main__':
