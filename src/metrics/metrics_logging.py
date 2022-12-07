8ebe84b36094283bd4bdae191e4b267ebc7d6c78
fmeyer <fabian.meyer@inovex.de>
2022-12-07 14:14:45 +0100

    change logging

--- src/metrics/metrics_logging.py
+++ src/metrics/metrics_logging.py
@@ -1,15 +1,23 @@
 import wandb
+from datetime import datetime
 from ood_detection.config import Config
 
 
-def wandb_log(metrics_dict):
-    wandb.init(project="thesis-datasets", entity="wandbefab")
-    wandb.config = {
-        "batch_size": 512,
-        "clip_model": Config.VISION_MODEL,
-        "device": Config.DEVICE,
-    }
+def wandb_log(metrics_dict, experiment="distances"):
+    if experiment == "distances":
+        wandb.init(project="thesis-datasets",
+                   entity="wandbefab",
+                   name="-".join([metrics_dict["name"], datetime.today().strftime('%Y/%m/%d')]),
+                   tags=['zeroshot', 'mmd', 'clp', 'zsa'],
+                   )
+        wandb.config = {
+            "batch_size": 512,
+            "clip_model": Config.VISION_MODEL,
+            "device": Config.DEVICE,
+        }
 
+    else:
+        return
     wandb.log(
         metrics_dict
     )
