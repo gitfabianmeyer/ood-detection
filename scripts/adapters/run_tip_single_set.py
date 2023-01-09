ef6b0a479791f9ec81eedde3019cccef23311585
fmeyer <fabian.meyer@inovex.de>
2023-01-10 00:52:21 +0100

    run all

--- scripts/adapters/run_tip_single_set.py
+++ scripts/adapters/run_tip_single_set.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 import logging
 import os
+
+import wandb
+from datasets.config import DATASETS_DICT
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from adapters.tip_adapter import clip_tip_adapter
-
 
 # import for clearml
 import clip
@@ -18,53 +21,28 @@ logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
 run_clearml = False
 
-cifar_templates = [
-    'a photo of a {}.',
-    'a blurry photo of a {}.',
-    'a black and white photo of a {}.',
-    'a low contrast photo of a {}.',
-    'a high contrast photo of a {}.',
-    'a bad photo of a {}.',
-    'a good photo of a {}.',
-    'a photo of a small {}.',
-    'a photo of a big {}.',
-    'a photo of the {}.',
-    'a blurry photo of the {}.',
-    'a black and white photo of the {}.',
-    'a low contrast photo of the {}.',
-    'a high contrast photo of the {}.',
-    'a bad photo of the {}.',
-    'a good photo of the {}.',
-    'a photo of the small {}.',
-    'a photo of the big {}.',
-]
-
-
-class OodCifar10(torchvision.datasets.CIFAR10):
-    def __init__(self, data_path, transform, train, templates=None):
-        super(OodCifar10, self).__init__(root=data_path,
-                                         transform=transform,
-                                         train=train,
-                                         download=True
-                                         )
-        self.targets = np.array(self.targets)
-        self.templates = templates if templates else cifar_templates
-        self.idx_to_class = {value: key for (key, value) in self.class_to_idx.items()}
-
 
 def main():
-    _logger.info("Starting cifar run")
-    dataset = OodCifar10
+    for dname, dset in DATASETS_DICT:
+        _logger.info(f"Starting {dname} run...")
 
-    results = clip_tip_adapter(dataset=dataset)
-    print(results)
-    return results
+        name = "-".join([dname, 'adapter', datetime.today().strftime('%Y/%m/%d')])
+        run = wandb.init(project="thesis-zsoodd-corruptions",
+                         entity="wandbefab",
+                         name=name)
+        try:
+            results = clip_tip_adapter(dataset=dset)
+            run.log(results)
+        except:
+            pass
+        run.finish()
 
 
 if __name__ == '__main__':
 
     if run_clearml:
         from clearml import Task
+
         print("running clearml")
         task = Task.init(project_name="ma_fmeyer", task_name="tip adapter testing")
         task.execute_remotely('5e62040adb57476ea12e8593fa612186')
