a189e287b2e681ad97512487c72c83672ce06b22
fmeyer <fabian.meyer@inovex.de>
2023-01-09 17:27:51 +0100

    add to root

---
+++ run_tip_single_set_copy.py
@@ -0,0 +1,66 @@
+from clearml import Task
+from adapters.tip_adapter import ClipTipAdapter
+
+# import for clearml
+import clip
+import torch
+import torchvision
+import numpy as np
+import sklearn
+import tqdm
+
+run_clearml = True
+
+cifar_templates = [
+    'a photo of a {}.',
+    'a blurry photo of a {}.',
+    'a black and white photo of a {}.',
+    'a low contrast photo of a {}.',
+    'a high contrast photo of a {}.',
+    'a bad photo of a {}.',
+    'a good photo of a {}.',
+    'a photo of a small {}.',
+    'a photo of a big {}.',
+    'a photo of the {}.',
+    'a blurry photo of the {}.',
+    'a black and white photo of the {}.',
+    'a low contrast photo of the {}.',
+    'a high contrast photo of the {}.',
+    'a bad photo of the {}.',
+    'a good photo of the {}.',
+    'a photo of the small {}.',
+    'a photo of the big {}.',
+]
+
+
+class OodCifar10(torchvision.datasets.CIFAR10):
+    def __init__(self, data_path, transform, train, templates=None):
+        super(OodCifar10, self).__init__(root=data_path,
+                                         transform=transform,
+                                         train=train,
+                                         download=True
+                                         )
+        self.targets = np.array(self.targets)
+        self.templates = templates if templates else cifar_templates
+        self.idx_to_class = {value: key for (key, value) in self.class_to_idx.items()}
+
+
+def main():
+    dataset = OodCifar10
+
+    tip_adapter = ClipTipAdapter(dataset=dataset,
+                                 kshots=16,
+                                 augment_epochs=1,
+                                 lr=0.001,
+                                 eps=1e-4)
+
+    result = tip_adapter.compare()
+    return result
+
+
+if __name__ == '__main__':
+    if run_clearml:
+        task = Task.init(project_name="ma_fmeyer", task_name="tip adapter testing")
+        task.execute_remotely('5e62040adb57476ea12e8593fa612186')
+
+    main()
