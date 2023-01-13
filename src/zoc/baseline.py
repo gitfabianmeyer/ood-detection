79d7e3801266e8ecf91558eebd1a124afda07a36
fmeyer <fabian.meyer@inovex.de>
2023-01-13 19:57:30 +0100

    add baseline

---
+++ src/zoc/baseline.py
@@ -0,0 +1,109 @@
+import logging
+from collections import defaultdict
+
+import numpy as np
+import torch
+import wandb
+from datasets.zoc_loader import IsolatedClasses
+from ood_detection.classification_utils import zeroshot_classifier
+from tqdm import tqdm
+from zoc.utils import get_ablation_splits, get_split_specific_targets, fill_auc_lists, fill_f_acc_lists, \
+    get_result_mean_dict
+
+_logger = logging.getLogger(__name__)
+
+
+@torch.no_grad()
+def get_feature_weight_dict(isolated_classes, clip_model, device):
+    weights_dict = {}
+    for cls in isolated_classes.classes:
+        loader = isolated_classes[cls]
+        loader.batch_size = 512
+        image_feature_list = []
+        for images in tqdm(loader):
+            images = images.to(device)
+            image_features = clip_model.encode_image(images)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+
+            image_feature_list.append(image_features)
+        weights_dict[cls] = torch.cat(image_feature_list).half()
+
+    return weights_dict
+
+
+def get_zeroshot_weight_dict(isolated_classes, clip_model):
+    weights_dict = {}
+    weights = zeroshot_classifier(isolated_classes.classes, isolated_classes.templates, clip_model)
+
+    for classname, weight in zip(isolated_classes.classes, weights):
+        weights_dict[classname] = weight
+
+    return weights_dict
+
+
+def sorted_zeroshot_weights(weights, split):
+    sorted_weights = []
+    for classname in split:
+        sorted_weights.append(weights[classname])
+    return torch.cat(sorted_weights)
+
+
+@torch.no_grad()
+def baseline_detector(clip_model,
+                      device,
+                      isolated_classes: IsolatedClasses = None,
+                      id_classes=6,
+                      ood_classes=4,
+                      runs=1, ):
+    feature_weight_dict = get_feature_weight_dict(isolated_classes, clip_model, device)
+    classes_weight_dict = get_zeroshot_weight_dict(isolated_classes, clip_model)
+
+    ablation_splits = get_ablation_splits(isolated_classes.labels, n=runs, id_classes=id_classes,
+                                          ood_classes=ood_classes)
+
+    metrics_list = []
+    # for each temperature..
+    for temperature in np.logspace(-7.158429362604483, 6.643856189774724, num=15,
+                                   base=2.0):  # 10 values between .007 and 100
+
+        auc_list_sum, auc_list_mean, auc_list_max = [], [], []
+        for split in ablation_splits:
+
+            seen_labels = split[:id_classes]
+            unseen_labels = split[id_classes:]
+            _logger.debug(f"Seen labels: {seen_labels}\nOOD Labels: {split[id_classes:]}")
+
+            zeroshot_weights = sorted_zeroshot_weights(classes_weight_dict, split)
+
+            ood_probs_sum, ood_probs_mean, ood_probs_max = [], [], []
+            f_probs_sum, acc_probs_sum, id_probs_sum = [], [], []
+
+            for i, semantic_label in enumerate(split):
+                _logger.info(f"Getting stats for label {semantic_label}")
+
+                image_features_for_label = feature_weight_dict[semantic_label]
+                zeroshot_probs = (temperature * image_features_for_label @ zeroshot_weights.T).softmax(dim=-1).squeeze()
+
+                # detection score is accumulative sum of probs of generated entities
+                ood_prob_sum = np.sum(zeroshot_probs[id_classes:].detach().cpu().numpy())
+                ood_probs_sum.append(ood_prob_sum)
+
+                ood_prob_mean = np.mean(zeroshot_probs[id_classes:].detach().cpu().numpy())
+                ood_probs_mean.append(ood_prob_mean)
+
+                top_prob, _ = zeroshot_probs.cpu().topk(1, dim=-1)
+                ood_probs_max.append(top_prob.detach().numpy())
+
+                id_probs_sum.append(1. - ood_prob_sum)
+
+            targets = get_split_specific_targets(isolated_classes, seen_labels, unseen_labels)
+            fill_auc_lists(auc_list_max, auc_list_mean, auc_list_sum, ood_probs_mean, ood_probs_max, ood_probs_sum,
+                           targets)
+            fill_f_acc_lists(acc_probs_sum, f_probs_sum, id_probs_sum, ood_probs_sum, targets)
+
+        metrics = get_result_mean_dict(acc_probs_sum, auc_list_max, auc_list_mean, auc_list_sum, f_probs_sum)
+        metrics["temperature"] = temperature
+
+        metrics_list.append(metrics)
+
+    return metrics_list
