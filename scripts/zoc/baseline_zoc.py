d206161ebab4919ee751564ff104a64f72313b65
fmeyer <fabian.meyer@inovex.de>
2023-01-13 20:06:01 +0100

    remove unnecessary params

--- scripts/zoc/baseline_zoc.py
+++ scripts/zoc/baseline_zoc.py
@@ -1,50 +1,25 @@
 import os
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from zoc.baseline import baseline_detector
-
-
 import argparse
 import logging
 
 import clip
-from clip.simple_tokenizer import SimpleTokenizer
 
 import torch
 import wandb
 from clearml import Task
-from datasets.config import DATASETS_DICT, HalfOneDict
+from datasets.config import DATASETS_DICT
 from datasets.zoc_loader import IsolatedClasses
-from metrics.metrics_logging import wandb_log
 from ood_detection.config import Config
-from transformers import BertGenerationTokenizer, BertGenerationConfig, BertGenerationDecoder
-from zoc.utils import image_decoder
 
 _logger = logging.getLogger(__name__)
 splits = [(.4, .6), ]
 clearml_model = False
 MODEL_PATH = "/home/fmeyer/ZOC/trained_models/COCO/ViT-B32/"
-
-
-def get_decoder():
-    if clearml_model:
-        artifact_task = Task.get_task(project_name='ma_fmeyer', task_name='Train Decoder')
-
-        model_path = artifact_task.artifacts['model'].get_local_copy()
-    else:
-        model_path = MODEL_PATH
-
-    bert_config = BertGenerationConfig.from_pretrained("google/bert_for_seq_generation_L-24_bbc_encoder")
-    bert_config.is_decoder = True
-    bert_config.add_cross_attention = True
-    bert_model = BertGenerationDecoder.from_pretrained('google/bert_for_seq_generation_L-24_bbc_encoder',
-                                                       config=bert_config).to(Config.DEVICE).eval()
-
-    bert_model.load_state_dict(
-        torch.load(model_path + 'model_3.pt', map_location=torch.device(Config.DEVICE))['net'])
-
-    return bert_model
 
 
 def run_single_dataset_ood(isolated_classes, name, clip_model, id_classes=.6, runs=5):
@@ -73,10 +48,6 @@ def run_single_dataset_ood(isolated_classes, name, clip_model, id_classes=.6, ru
 def run_all(args):
     # for each dataset
     clip_model, clip_transform = clip.load(Config.VISION_MODEL)
-    bert_tokenizer = BertGenerationTokenizer.from_pretrained('google/bert_for_seq_generation_L-24_bbc_encoder')
-    clip_tokenizer = SimpleTokenizer()
-    bert_model = get_decoder()
-
     for dname, dset in DATASETS_DICT.items():
 
         if dname != 'cifar10':
@@ -106,9 +77,6 @@ def run_all(args):
             run_single_dataset_ood(isolated_classes=isolated_classes,
                                    name=dname,
                                    clip_model=clip_model,
-                                   clip_tokenizer=clip_tokenizer,
-                                   bert_tokenizer=bert_tokenizer,
-                                   bert_model=bert_model,
                                    id_classes=split[0],
                                    runs=args.runs_ood)
 
