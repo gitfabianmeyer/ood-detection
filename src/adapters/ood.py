0e4987de3b3dd75f383ac7c7bdd978137e4bb15c
fmeyer <fabian.meyer@inovex.de>
2023-01-27 21:30:15 +0100

    correct import

--- src/adapters/ood.py
+++ src/adapters/ood.py
@@ -1,15 +1,15 @@
 import logging
 
 import torch
-from adapters.tip_adapter import get_cache_logits, get_cache_model, create_tip_train_set, \
-    load_hyperparams_from_training, search_hp
+from adapters.tip_adapter import get_cache_logits, get_cache_model,\
+    create_tip_train_set, load_hyperparams_from_training, \
+    search_hp, get_dataset_with_shorted_classes, \
+    get_dataset_features_from_dataset_with_split
 from datasets.zoc_loader import IsolatedClasses
 from ood_detection.config import Config
 from torch.utils.data import DataLoader
 from zoc.baseline import sorted_zeroshot_weights, get_zeroshot_weight_dict, get_feature_weight_dict
 from zoc.utils import get_mean_std, get_auroc_for_max_probs, get_split_specific_targets, get_ablation_splits
-
-from src.adapters.tip_adapter import get_dataset_with_shorted_classes, get_dataset_features_from_dataset_with_split
 
 _logger = logging.getLogger(__name__)
 
@@ -62,7 +62,7 @@ def tip_hyperparam_ood_detector(dset,
         cache_keys, cache_values = get_cache_model(tip_train_set, clip_model, augment_epochs=augment_epochs)
 
         # get shorted val set for the
-        tip_val_set = get_dataset_with_shorted_classes(dset, seen_labels, kshots, 'val')
+        tip_val_set = get_dataset_with_shorted_classes(dset, seen_labels, 'val')
         shorted_val_loader = DataLoader(tip_val_set, batch_size=512)
         # get features from the shorted val set
         val_features, val_labels, label_features, classes = get_dataset_features_from_dataset_with_split(
