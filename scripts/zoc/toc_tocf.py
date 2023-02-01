bea93938a7f6a607b98997ae368ed88de974be97
fmeyer <fabian.meyer@inovex.de>
2023-02-01 10:55:53 +0100

    uncomment changes, try with dtd

--- scripts/zoc/toc_tocf.py
+++ scripts/zoc/toc_tocf.py
@@ -31,10 +31,9 @@ def main():
     failed = []
     clip_model, clip_transform = clip.load(Config.VISION_MODEL)
     device = Config.DEVICE
-    # bert_tokenizer = BertGenerationTokenizer.from_pretrained('google/bert_for_seq_generation_L-24_bbc_encoder')
-    # clip_tokenizer = SimpleTokenizer()
-    # bert_model = get_decoder()
-    bert_tokenizer, clip_tokenizer, bert_model = None, None, None
+    bert_tokenizer = BertGenerationTokenizer.from_pretrained('google/bert_for_seq_generation_L-24_bbc_encoder')
+    clip_tokenizer = SimpleTokenizer()
+    bert_model = get_decoder()
 
     for dname, dset in HalfOneDict.items():
         if dname != 'dtd':
