3f2a4a1016a50f2af608a0dd40cf2ff4aa463187
fmeyer <fabian.meyer@inovex.de>
2023-02-03 17:00:56 +0100

    rename tip adapter dataset

--- scripts/zoc/cifar100_ablation_splits.py
+++ scripts/zoc/cifar100_ablation_splits.py
@@ -38,7 +38,7 @@ train_epochs = 20
 augment_epochs = 10
 lr = 0.001
 eps = 1e-4
-id_splits = np.linspace(0.01, 0.99, 25)
+id_splits = np.linspace(0.02, 0.99, 25)
 
 
 def main():
@@ -145,7 +145,7 @@ def adapter_zoc_ablation(dset,
 
             # get the kshot train set
             tip_train_set = create_tip_train_set(dset, seen_labels, kshots)
-            tip_train_set.name = f"{tip_train_set.name}_{runs_per_setting}-runs_ood_split-{split_idx}"
+            tip_train_set.name = f"{tip_train_set.name}_splits_ablation"
             _logger.info(f"len train set: {len(tip_train_set)}. Should be: {len(tip_train_set.classes) * kshots} (max)")
             cache_keys, cache_values = get_cache_model(tip_train_set, clip_model, augment_epochs=augment_epochs)
 
