834638c8118b3c30d0ddb2b5066b148b71f68ce6
fmeyer <fabian.meyer@inovex.de>
2023-02-06 11:17:12 +0100

    gpu selection

--- scripts/zoc/cifar100_ablation_splits.py
+++ scripts/zoc/cifar100_ablation_splits.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 
 import logging
 from collections import defaultdict
