9a4cc98229788344c92715459143489951d2ccde
fmeyer <fabian.meyer@inovex.de>
2023-02-09 19:50:03 +0100

    minor adjustments

--- scripts/ablation/cifar100_ablation_linear_ood.py
+++ scripts/ablation/cifar100_ablation_linear_ood.py
@@ -79,7 +79,7 @@ def get_trained_linear_classifier(train_set, val_set, seen_labels, clip_model=No
     val_set_loaders = IsolatedClasses(val_set)
 
     feature_weight_dict_train = get_feature_weight_dict(train_set_loaders, clip_model, device)
-    feature_weight_dict_val = get_feature_weight_dict(val_set_loaders)
+    feature_weight_dict_val = get_feature_weight_dict(val_set_loaders, clip_model, device)
     linear_train_set = FeatureSet(feature_weight_dict_train, seen_labels, class_to_idx_mapping)
     linea_val_set = FeatureSet(feature_weight_dict_val, seen_labels, class_to_idx_mapping)
 
