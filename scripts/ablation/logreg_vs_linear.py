8bbcab3e1c06be95522dc9634ae70a48fc9455ff
fmeyer <fabian.meyer@inovex.de>
2023-02-13 13:37:51 +0100

    fix logreg

--- scripts/ablation/logreg_vs_linear.py
+++ scripts/ablation/logreg_vs_linear.py
@@ -1,3 +1,8 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+
 import logging
 
 import clip
@@ -22,6 +27,7 @@ def main():
     for dname, dset in DATASETS_DICT.items():
         _logger.info(dname)
         log_reg_stuff(dset, clip_model, clip_transfrom, 10, 1)
+        break
 
 
 def log_reg_stuff(dataset, clip_model, clip_transform, id_classes, runs):
@@ -33,34 +39,28 @@ def log_reg_stuff(dataset, clip_model, clip_transform, id_classes, runs):
     ood_classes = len(train_dataset.classes) - id_classes
     isolated_classes = IsolatedClasses(train_dataset,
                                        batch_size=512)
-    # feature_weight_dict_train = get_feature_weight_dict(isolated_classes, clip_model, device)
+    feature_weight_dict_train = get_feature_weight_dict(isolated_classes, clip_model, device)
 
-    classes = ["1", "2", "3", "4", "5"]
-    feature_weight_dict_train = {key: torch.rand((5,10)) for key in classes}
+    classes = train_dataset.classes
     isolated_classes = IsolatedClasses(dataset(Config.DATAPATH,
                                                split='val',
                                                transform=clip_transform),
                                        batch_size=512)
-    # feature_weight_dict_val = get_feature_weight_dict(isolated_classes, clip_model, device)
-    feature_weight_dict_val = {key: torch.rand((5, 10)) for key in classes}
-    ablation_splits = get_ablation_splits(isolated_classes.classes, n=1, id_classes=id_classes, # TODO
+    feature_weight_dict_val = get_feature_weight_dict(isolated_classes, clip_model, device)
+    ablation_splits = get_ablation_splits(isolated_classes.classes, n=runs, id_classes=id_classes,
                                           ood_classes=ood_classes)
 
     logistic_aucs, linear_aucs = [], []
     for ablation_split in ablation_splits:
 
-        # class_to_idx_mapping = {label: i for (i, label) in enumerate(ablation_split)}
-        # seen_labels = ablation_split[:id_classes]
-        # unseen_labels = ablation_split[id_classes:]
-        seen_labels = classes[:3]
-        unseen_labels = classes[3:]
-        class_to_idx_mapping = {key:i for (i, key) in enumerate(classes)}
+        class_to_idx_mapping = {label: i for (i, label) in enumerate(ablation_split)}
+        seen_labels = ablation_split[:id_classes]
+        unseen_labels = ablation_split[id_classes:]
         _logger.info(f"Seen labels: {seen_labels}\nOOD Labels: {unseen_labels}")
 
         # train classifier to classify id set
         train_set = FeatureSet(feature_weight_dict_train, seen_labels, class_to_idx_mapping)
         val_set = FeatureSet(feature_weight_dict_val, seen_labels, class_to_idx_mapping)
-
 
         logistic_classifier = train_log_reg_classifier(train_set, val_set)
         linear_classifier = train_id_classifier(train_set, val_set, wandb_logging=False)
@@ -70,8 +70,7 @@ def log_reg_stuff(dataset, clip_model, clip_transform, id_classes, runs):
                                                    split='test',
                                                    transform=clip_transform),
                                            batch_size=512)
-        # feature_weight_dict_test = get_feature_weight_dict(isolated_classes, clip_model, device) # TODO
-        feature_weight_dict_test = {key: torch.rand((5, 10)) for key in classes}
+        feature_weight_dict_test = get_feature_weight_dict(isolated_classes, clip_model, device)
 
         logreg_probs_max, linear_probs_max = [], []
         for i, semantic_label in enumerate(ablation_split):
@@ -80,11 +79,12 @@ def log_reg_stuff(dataset, clip_model, clip_transform, id_classes, runs):
             # calc the logits and softmaxs
             linear_logits = linear_classifier(image_features_for_label.to(torch.float32).to(device))
             top_prob, _ = linear_logits.cpu().topk(1, dim=-1)
-            _, logistic_logits = torch.topk(torch.Tensor(logistic_classifier.predict_proba(image_features_for_label)),
-                                            1, 1)
 
-            assert linear_logits.shape[1] == id_classes
-            assert logistic_logits.shape[1] == id_classes
+            logistic_logits = logistic_classifier.predict_proba(image_features_for_label)
+            top_log_logits, _ = torch.topk(torch.Tensor(logistic_logits), 1, -1)
+
+            assert linear_logits.shape[1] == id_classes, f"{linear_logits.shape} and {id_classes}"
+            assert top_log_logits.shape[1] == id_classes, f"{top_log_logits.shape} and {id_classes}"
 
             # detection score is accumulative sum of probs of generated entities
             # careful, only for this setting axis=1
@@ -95,11 +95,12 @@ def log_reg_stuff(dataset, clip_model, clip_transform, id_classes, runs):
 
         linear_aucs.append(get_auroc_for_max_probs(targets, linear_probs_max))
         logistic_aucs.append(get_auroc_for_max_probs(targets, logreg_probs_max))
-
+        break
 
     print(f" linear: {np.mean(linear_aucs)}")
     print(f" logistic: {np.mean(logistic_aucs)}")
     return True
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
