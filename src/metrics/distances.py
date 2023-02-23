bcc4ad9f0d41ff3a55c89fb6be3cbc1a0b72136d
fmeyer <fabian.meyer@inovex.de>
2023-02-23 13:17:24 +0100

    try on cpu

--- src/metrics/distances.py
+++ src/metrics/distances.py
@@ -227,7 +227,7 @@ def get_far_clp(id_dict: FeatureDict, ood_dict: FeatureDict, clip_model, tempera
 
 def get_mmd_rbf_kernel(id_features, ood_features):
     import math
-    X = torch.cat((id_features, ood_features)).to(Config.DEVICE)
+    X = torch.cat((id_features, ood_features)).cpu()
     print(X.shape)
     cd = torch.cdist(X, X)
     print(cd)
