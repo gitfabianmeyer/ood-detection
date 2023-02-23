e6c34785ad1ae7601bccdd8c614f3a224b1c40cf
fmeyer <fabian.meyer@inovex.de>
2023-02-23 12:12:51 +0100

    add debugging,

--- src/metrics/distances.py
+++ src/metrics/distances.py
@@ -228,7 +228,11 @@ def get_far_clp(id_dict: FeatureDict, ood_dict: FeatureDict, clip_model, tempera
 def get_mmd_rbf_kernel(id_features, ood_features):
     import math
     X = torch.cat((id_features, ood_features)).to(Config.DEVICE)
-    kernel_size = torch.mean(torch.cdist(X, X)).cpu().numpy()
+    print(X.shape)
+    cd = torch.cdist(X, X)
+    print(cd)
+    print(cd.shape)
+    kernel_size = torch.mean(cd).cpu().numpy()
     float_kernel = float(kernel_size)
     if math.isnan(float_kernel):
         raise ValueError("Kernel is NAN")
