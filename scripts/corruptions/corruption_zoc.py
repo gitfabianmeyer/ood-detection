ba8669d63f9e4aaedd091065a88f7ff4fa352141
fmeyer <fabian.meyer@inovex.de>
2023-03-13 01:28:24 +0100

    adjust to zsoodd

--- scripts/corruptions/corruption_zoc.py
+++ scripts/corruptions/corruption_zoc.py
@@ -3,27 +3,22 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-
-def run_single_dataset_ood(isolated_classes, clip_model, clip_tokenizer, bert_tokenizer, bert_model,
-                           id_split, runs):
-
+def run_single_dataset_ood(isolated_classes, clip_model, clip_tokenizer, bert_tokenizer, bert_model, runs, shorten):
+    from ood_detection.config import Config
     from zoc.detectors import zoc_detector
-
-
-    labels = list(isolated_classes.keys())
-    _logger.info(f'Running with classes {labels[:10]} ...')
 
     metrics = zoc_detector(isolated_classes,
                            clip_model,
                            clip_tokenizer,
                            bert_tokenizer,
                            bert_model,
-                           id_split,
-                           runs=runs)
+                           Config.ID_SPLIT,
+                           runs,
+                           shorten_classes=None if shorten == 0 else shorten)
     return metrics
 
 
-def main(args):
+def run_all(args):
     import numpy as np
 
     from datasets.corruptions import get_corruption_transform, THESIS_CORRUPTIONS
@@ -35,19 +30,20 @@ def main(args):
     from ood_detection.config import Config
     from transformers import BertGenerationTokenizer
     from zoc.utils import get_decoder
+
     # for each dataset
     clip_model, clip_transform = clip.load(Config.VISION_MODEL)
     bert_tokenizer = BertGenerationTokenizer.from_pretrained('google/bert_for_seq_generation_L-24_bbc_encoder')
     clip_tokenizer = SimpleTokenizer()
-
-    _logger.info('Loading decoder model')
     bert_model = get_decoder()
 
-    if args.max_splits !=0:
-        splits = np.array_split(list(CorruptionSets.keys()), args.max_splits)
-        datasets = splits[args.spllit -1]
-    else:
+    if args.split == 0:
         datasets = CorruptionSets.keys()
+    else:
+        datasets_splits = np.array_split(list(CorruptionSets.keys()), args.max_split)
+        datasets = datasets_splits[args.split - 1]
+        _logger.info(f"Current split: {args.split}: {datasets}")
+
     for dname in datasets:
         dset = CorruptionSets[dname]
         for cname, ccorr in THESIS_CORRUPTIONS.items():
@@ -59,19 +55,19 @@ def main(args):
                                    'metrics'])
             for severity in [1, 3, 5]:
                 _logger.info(f"Running {dname} with {cname} and severity {severity}")
-                transform = get_corruption_transform(clip_transform, ccorr, severity)
-                isolated_classes = IsolatedClasses(dataset=dset(data_path=Config.DATAPATH,
-                                                                split='test',
-                                                                transform=transform),
+                corruption_transform = get_corruption_transform(clip_transform, ccorr, severity)
+                dataset = dset(data_path=Config.DATAPATH,
+                               split='test',
+                               transform=corruption_transform),
+                isolated_classes = IsolatedClasses(dataset,
                                                    batch_size=512
                                                    )
-                    # perform zsoodd
+                # perform zsoodd
                 metrics_dict = run_single_dataset_ood(isolated_classes=isolated_classes,
                                                       clip_model=clip_model,
                                                       clip_tokenizer=clip_tokenizer,
                                                       bert_tokenizer=bert_tokenizer,
                                                       bert_model=bert_model,
-                                                      id_split=Config.ID_SPLIT,
                                                       runs=args.runs)
                 metrics_dict['severity'] = severity
 
@@ -80,7 +76,7 @@ def main(args):
             run.finish()
 
 
-if __name__ == '__main__':
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -92,3 +88,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     main(args)
+
+
+if __name__ == '__main__':
+    main()
